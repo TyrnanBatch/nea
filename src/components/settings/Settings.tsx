431581b036e268bd318d99e3f922fa45bced69c6
tyrnanbatch <tyrnan.batch@gmail.com>
2023-10-10 14:15:54 +0100

    further developing gui

--- src/components/settings/Settings.tsx
+++ src/components/settings/Settings.tsx
@@ -4,8 +4,12 @@ import './Settings.css';
 export default function Settings() {
     return (
         <div className={"setting-container"}>
-            <div>Enter your data to sort separated by a ','</div>
-            <input className={"data-input"} placeholder={"e.g. 37, 28, 68, 26"}/>
+            <div className={"data-input"}>
+                <div className={"data-input-text"}>
+                    Enter your data to sort separated by a ','
+                </div>
+                <input placeholder={"e.g. 37, 28, 68, 26"}/>
+            </div>
         </div>
     );
 }
