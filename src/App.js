953bda4b19cf61d7d43dbdeb61fc19eef83172ce
tyrnanbatch <tyrnan.batch@gmail.com>
2024-03-15 12:23:09 +0000

    indentation

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import './App.css';
 
 function App() {
-  return (
-    <div>
-      Home Page
-    </div>
-  );
+    return (
+        <div>
+            Home Page
+        </div>
+    );
 }
 
 export default App;
