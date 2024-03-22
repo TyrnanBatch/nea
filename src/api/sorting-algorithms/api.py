25924b6de73b30f756146c769c32cbcc0de2c1d3
tyrnanbatch <tyrnan.batch@gmail.com>
2024-03-22 23:23:09 +0000

    added python rest api

---
+++ src/api/sorting-algorithms/api.py
@@ -0,0 +1,27 @@
+from flask import Flask, request, jsonify
+
+from bubble_sort import BubbleSort
+from insertion_sort import InsertionSort
+from merge_sort import MergeSort
+
+app = Flask(__name__)
+
+
+@app.route('/sort', methods=['POST'])
+def sort():
+    request_data = request.json
+    input_data = request_data.get("input_data")
+    algorithms = request_data.get("algorithms")
+
+    if algorithms == "insertion":
+        return jsonify({"result": InsertionSort.sort(input_data)})
+    elif algorithms == "bubble":
+        return jsonify({"result": BubbleSort.sort(input_data)})
+    elif algorithms == "merge":
+        return jsonify({"result": MergeSort.sort(input_data)})
+
+    return jsonify({"error": "Error at /sort http request"}), 400
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
